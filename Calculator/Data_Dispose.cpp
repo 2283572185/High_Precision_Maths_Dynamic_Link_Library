7d4a2c8caa68e0c6dab147bc3131f34f1b1b1baa
2283572185 <2283572185@qq.com>
2020-08-16 22:07:02 +0800

    创建计算器项目

---
+++ Calculator/Data_Dispose.cpp
@@ -0,0 +1,34 @@
+﻿// Data_Dispose.cpp: 实现文件
+//
+
+#include "pch.h"
+#include "Calculator.h"
+#include "Data_Dispose.h"
+#include "afxdialogex.h"
+
+
+// Data_Dispose 对话框
+
+IMPLEMENT_DYNAMIC(Data_Dispose, CDialogEx)
+
+Data_Dispose::Data_Dispose(CWnd* pParent /*=nullptr*/)
+	: CDialogEx(IDD_CALCULATOR_DIALOG, pParent)
+{
+
+}
+
+Data_Dispose::~Data_Dispose()
+{
+}
+
+void Data_Dispose::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+
+BEGIN_MESSAGE_MAP(Data_Dispose, CDialogEx)
+END_MESSAGE_MAP()
+
+
+// Data_Dispose 消息处理程序
---
+++ Calculator/resource.h
@@ -0,0 +1,43 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ 生成的包含文件。
+// 供 Calculator.rc 使用
+//
+#define IDD_CALCULATOR_DIALOG           102
+#define IDR_MAINFRAME                   128
+#define IDC_EDIT2                       1001
+#define IDC_BUTTON1                     1002
+#define IDC_BUTTON2                     1003
+#define IDC_BUTTON3                     1004
+#define IDC_BUTTON4                     1005
+#define IDC_BUTTON5                     1006
+#define IDC_BUTTON6                     1007
+#define IDC_BUTTON7                     1008
+#define IDC_BUTTON8                     1009
+#define IDC_BUTTON9                     1010
+#define IDC_BUTTON10                    1011
+#define IDC_BUTTON11                    1012
+#define IDC_BUTTON12                    1013
+#define IDC_BUTTON13                    1014
+#define IDC_BUTTON14                    1015
+#define IDC_BUTTON15                    1016
+#define IDC_BUTTON16                    1017
+#define IDC_BUTTON17                    1018
+#define IDC_BUTTON18                    1019
+#define IDC_BUTTON19                    1020
+#define IDC_BUTTON20                    1021
+#define IDC_BUTTON21                    1022
+#define IDC_BUTTON22                    1023
+#define IDC_BUTTON23                    1024
+#define IDC_BUTTON24                    1025
+#define IDC_HOTKEY1                     1026
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        130
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1027
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
